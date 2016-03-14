081cee092581dfb9f1334a602e8c05221e33c775
pgreen1014 <pgreen.78633@gmail.com>
2016-03-14 01:31:02 -0500

    added setMonthlyExpense() method

--- app/src/main/java/helperClasses/FinanceDataHelper.java
+++ app/src/main/java/helperClasses/FinanceDataHelper.java
@@ -1,6 +1,9 @@
 package helperClasses;
 
 
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
 /**
  * Created by Philip on 3/12/2016.
  */
@@ -8,6 +11,22 @@ public class FinanceDataHelper {
 
     public static double amountRemaining(double total, double amount) {
         return total - amount;
+    }
+
+    public static String setMonthlyExpense(int income, int percent, double amountSpent){
+        double monthlyIncome = income;
+        //convert percent to decimal form
+        double percentDecimal = percent/100.0;
+
+        double totalToSpend = monthlyIncome*percentDecimal;
+
+        double totalRemaining = totalToSpend - amountSpent;
+
+        DecimalFormat df = new DecimalFormat("#0.00");
+        df.setRoundingMode(RoundingMode.DOWN);
+
+        String result = df.format(totalRemaining);
+        return result;
     }
 
 }
