c43d8224ecb834eb499ccedc44ac97c944d5f052
pgreen1014 <pgreen.78633@gmail.com>
2016-04-26 01:23:26 -0500

    removed setMonthlyExpense() amountRemaining() setSpendingTotal() and getMonthlyExpense()from FinanceDataHelper.java

--- app/src/main/java/helperClasses/FinanceDataHelper.java
+++ app/src/main/java/helperClasses/FinanceDataHelper.java
@@ -14,29 +14,6 @@ import java.text.DecimalFormat;
  */
 public class FinanceDataHelper {
 
-    //calculate the total amount left to spend for the month
-    public static double amountRemaining(double total, double amount) {
-        return total - amount;
-    }
-
-    //returns amount remaining to spend for the month on startup
-    public static String setMonthlyExpense(String income, String percent, String amountSpent){
-        //Convert Parameters to BigDecimal
-        BigDecimal percentage = new BigDecimal(percent);
-        BigDecimal monthlyIncome = new BigDecimal(income);
-        BigDecimal spent = new BigDecimal(amountSpent);
-
-        //calculate percentage of total income to spend
-        BigDecimal totalToSpend = monthlyIncome.multiply(percentage);
-        //Make sure decimal places are limited to 2
-        totalToSpend = totalToSpend.setScale(2);
-
-        //calculate total remaining to spend for the month
-        BigDecimal result = totalToSpend.subtract(spent);
-
-        return result.toString();
-    }
-
     //returns the monthly amount remaining to spend after user added data to the Parse database
     public static String returnTotalRemaining(CharSequence amountRemaining, String expense){
         //Convert Parameters into BigDecimal
@@ -49,21 +26,5 @@ public class FinanceDataHelper {
         //return result as String
         return result.toString();
     }
-
-    //returns the total amount of expenditure left to spend
-    public static String setSpendingTotal(String parseCategory, Context context, String TAG){
-
-        double expenses = ParseHelper.getExpenditure(parseCategory);
-
-        if(expenses == 0) {
-            Toast.makeText(context, "Unable to Retrieve Data from Server", Toast.LENGTH_LONG).show();
-            Log.d(TAG, "no data retrieved from Parse server");
-            return Double.toString(expenses);
-        }
-        else{
-            return Double.toString(expenses);
-        }
-    }
-
 
 }
